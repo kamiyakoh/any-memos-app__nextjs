ddb0dcf830035047c008ff61a5c081c27c9e2262
kamiyakoh <nanayou791@gmail.com>
2024-04-22 01:20:09 +0900

    storybook導入

---
+++ .storybook/preview.ts
@@ -0,0 +1,22 @@
+import type { Preview } from '@storybook/react';
+import '../src/app/globals.css';
+import { MINIMAL_VIEWPORTS, INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+    viewport: {
+      viewports: {
+        ...MINIMAL_VIEWPORTS,
+        ...INITIAL_VIEWPORTS,
+      },
+    },
+  },
+};
+
+export default preview;
